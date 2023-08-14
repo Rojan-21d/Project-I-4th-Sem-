e536067c3afed8d0960ac88094155864755d46b7
rojan <roozandumaru@gmail.com>
2023-08-14 21:07:18 +0545

    reset password complete

--- forgotPassword/reset_password.php
+++ forgotPassword/reset_password.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $otp = $_SESSION['otp'];
 $otp_hash = hash("sha256", $otp);
 
@@ -54,15 +58,36 @@ if(isset($_POST["verify"])){
         $stmt->execute();
     
         if ($stmt->affected_rows > 0) {
-            echo "Password Changed";
+            // Password changed successfully
+            ?>
+            <script>
+                Swal.fire({
+                    title: "Success",
+                    text: "Password Changed",
+                    icon: "success",
+                    confirmButtonText: "OK"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = "login.php";
+                    }
+                });
+            </script>
+            <?php
         } else {
-            echo "Password change failed";
+            // Password change failed
+            ?>
+            <script>
+                Swal.fire({
+                    title: "Error",
+                    text: "Password change failed",
+                    icon: "error",
+                    confirmButtonText: "OK"
+                });
+            </script>
+            <?php
+        }
         }
     }
-    
-    echo "inside of if";
-}
-echo "outside of if";
 ?>
 
 <!DOCTYPE html>
@@ -72,135 +97,55 @@ echo "outside of if";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/7b1b8b2fa3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/login.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>    <!-- Include SweetAlert library from CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.all.min.js"></script>
     <title>Reset Password</title>
 </head>
 <body>
-    <form action="" method="post" class="login">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="login">    
         <div class="input-field ">
             <i class="fa-solid fa-key"></i>
             <input type="password" placeholder="Password *" name="password" id="password" required>
-            <i class="fa-regular fa-eye" id="togglePassword"></i>
+            <i class="fa-regular fa-eye toggle-password"></i>
         </div>  
         <div class="input-field ">
             <i class="fa-solid fa-key"></i>
             <input type="password" placeholder="Confirm Password *" name="password_confirmation" id="password_confirmation" required>
-            <i class="fa-regular fa-eye" id="togglePassword"></i>
+            <i class="fa-regular fa-eye toggle-password"></i>
         </div>  
         <button type="submit" name="verify">Reset Password</button>
     </form>
-    <script src="../js/showpwd.js"></script>    
+
+    <script src="../js/showpwd.js"></script>
     <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const passwordField = document.getElementById("password");
-        const confirmPasswordField = document.getElementById("password_confirmation");
-        const form = document.querySelector("form");
-
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-
-            const password = passwordField.value;
-            const confirmPassword = confirmPasswordField.value;
-            const errors = [];
-
-            if (password !== confirmPassword) {
-                errors.push("Passwords do not match.");
-            }
-            if (password.length < 8 || password.length > 24) {
-                errors.push("Password must be between 8 and 24 characters.");
-            }
-
-            if (errors.length > 0) {
-                const errorMessage = errors.join("\n");
-                Swal.fire("Error", errorMessage, "error");
-                return;
-            }
-
-            // If validation passes, submit the form
-            form.submit();
+        document.addEventListener("DOMContentLoaded", function () {
+            const form = document.querySelector(".login");
+            const passwordField = document.getElementById("password");
+            const confirmPasswordField = document.getElementById("password_confirmation");
+            
+            form.addEventListener("submit", function (event) {
+                event.preventDefault();
+                
+                const password = passwordField.value;
+                const confirmPassword = confirmPasswordField.value;
+                const errors = [];
+                
+                if (password !== confirmPassword) {
+                    errors.push("Passwords do not match.");
+                }
+                if (password.length < 8 || password.length > 24) {
+                    errors.push("Password must be between 8 and 24 characters.");
+                }
+                
+                if (errors.length > 0) {
+                    const errorMessage = errors.join("\n");
+                    Swal.fire("Error", errorMessage, "error");
+                    return;
+                }
+                
+                // If validation passes, submit the form
+                form.submit();
+            });
         });
-    });
-</script>
-
+    </script>
 </body>
 </html>
-
-
-
-<?php
-// $token = $_GET["token"];
-
-// $token_hash = hash("sha256", $token);
-
-// require '../backend/databaseconnection.php';
-
-// $table = ($userselects === "carrier") ? "carrierdetails" : "consignordetails";
-// $sql = "SELECT * FROM $table WHERE reset_token_hash = ?";
-// $stmt = $conn->prepare($sql);
-
-// $stmt = bind_param("s",$token_hash);
-
-// $stmt->execute();
-
-// $result = $stmt->get_result();
-// $user = $result->fetch_assoc();
-// if ($user === null) {
-//     die("<script>Swal.fire('Error', 'Token not found.', 'error');</script>");
-// }
-// if (strtotime($user["reset_token_expires_at"]) <= time()) {
-//     die("<script>Swal.fire('Error', 'Token has expired.', 'error');</script>");
-// }
-?>
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/7b1b8b2fa3.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/login.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
-    <title>Reset Password</title>
-</head>
-<body>
-    <form action="process_reset_password.php" method="post">
-        <div class="input-field ">
-            <i class="fa-solid fa-key"></i>
-            <input type="password" placeholder="Password *" name="password" id="password" required>
-            <i class="fa-regular fa-eye" id="togglePassword"></i>
-        </div>  
-        <div class="input-field ">
-            <i class="fa-solid fa-key"></i>
-            <input type="password" placeholder="Confirm Password *" name="password_confirmation" id="password_confirmation" required>
-            <i class="fa-regular fa-eye" id="togglePassword"></i>
-        </div>  
-        <input type="hidden" name="token" value="<?//= htmlspecialchars($token)?>">
-        <button>Send</button>
-    </form>
-    <script src="js/showpwd.js"></script>    
-    <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const passwordField = document.getElementById("password");
-        const confirmPasswordField = document.getElementById("password_confirmation");
-        const form = document.querySelector("form");
-
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-
-            const password = passwordField.value;
-            const confirmPassword = confirmPasswordField.value;
-
-            // Client-side validation
-            if (password !== confirmPassword) {
-                Swal.fire("Error", "Passwords do not match.", "error");
-                return;
-            }
-
-            // If validation passes, submit the form
-            form.submit();
-        });
-    });
-</script>
-
-</body>
-</html> -->
