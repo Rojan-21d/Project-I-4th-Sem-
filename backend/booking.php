ed516dffb196da3b033b13def113c2718d16d0c2
Rojan-21d <roozandumaru@gmail.com>
2023-10-15 09:48:48 +0545

    sweetalert finish 1

--- backend/booking.php
+++ backend/booking.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/sweetAlert.css">
+    <script src="../js/sweetalert.js"></script>
+    <title>Booking</title>
+</head>
+<body>
 <?php
 if (isset($_POST['action']) && isset($_POST['load_id']) && isset($_POST['consignor_id']) && isset($_POST['carrier_id'])) {
     $load_id = $_POST['load_id'];
@@ -17,14 +27,37 @@ if (isset($_POST['action']) && isset($_POST['load_id']) && isset($_POST['consign
         // Commit the transaction
         $conn->commit();
         
-        echo "<script>alert('Booking Successful');</script>";
-        echo "<script>window.location.href = '../home.php';</script>";
+        echo '<script>
+            Swal.fire({
+                title: "Booking Successful",
+                text: "Your load has been booked successfully.",
+                icon: "success",
+                confirmButtonText: "OK"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "../home.php";
+                }
+            });
+        </script>';
         exit;
     } catch (Exception $e) {
         // Rollback in case of error
         $conn->rollback();
-        echo "<script>alert('Error: ".$e->getMessage()."');</script>";
+        echo '<script>
+            Swal.fire({
+                title: "Error",
+                text: "Error: ' . $e->getMessage() . '",
+                icon: "error",
+                confirmButtonText: "OK"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "../home.php";
+                }
+            });
+        </script>';
         exit;
     }
 }
 ?>
+</body>
+</html>
